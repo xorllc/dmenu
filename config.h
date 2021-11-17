ec2ae3f98cf1f85d39f7545aa014733c139ef95f
John Doe <john.doe@example.com>
2021-11-16 20:58:16 -0500

    Make greens darker.

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#4e694d" },
+	[SchemeSel] = { "#eeeeee", "#394d38" },
 	[SchemeOut] = { "#000000", "#6b916a" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
