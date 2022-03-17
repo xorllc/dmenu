fbb543714f77907e9a3545f34401ded5f1649b01
John Doe <john.doe@example.com>
2022-03-16 21:26:06 -0400

    More configuration moving away from Xresources.

--- config.h
+++ config.h
@@ -4,8 +4,10 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10",
-	"NotoColorEmoji:pixelsize=8:antialias=true:autohint=true"
+	/* "monospace:size=10", */
+	/* "NotoColorEmoji:pixelsize=8:antialias=true:autohint=true" */
+	"monospace:size=11",
+	"JoyPixels:pixelsize=11:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
