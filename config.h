9974879ff1044c07a7a4d5e98133cf55c8783ab5
John Doe <john.doe@example.com>
2022-05-01 02:09:03 -0400

    Moving to more of a bluish color for the main foreground color instead of green.

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#394d38" },
+	[SchemeSel] = { "#eeeeee", "#344247" },
 	[SchemeOut] = { "#000000", "#6b916a" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
